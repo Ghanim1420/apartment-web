b68360b4fb8ab65b55f22173c9f6ebca2872d386
Ghanim1420 <ghanimghanim1111@gmail.com>
2026-09-15 08:45:11 +0300

    Update Supabase anon key in supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://pfitjbadugpbyijuieyl.supabase.co'
-const supabaseAnonKey = 'sb_publishable_Wu0M9r4KBANxPTCl3-Zo1A_2JEufVt3'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmaXRqYmFkdWdwYnlpanVpZXlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzNTQ5ODMsImV4cCI6MjEwNDkzMDk4M30.7kGsUENp6Bm3L-UWIQNPanG46I7mg5bbolE4RKoA4H0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
